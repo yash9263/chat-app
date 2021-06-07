7872e203f0277068ad35c8ff127560ee97c0ad9b
Yashwant <yashwant8530@gmail.com>
2021-06-07 13:10:00 +0530

    added illustartion

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,33 +3,34 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>Welcome to Let's Chat app.</h1>
-      <p>
-        Here you can create rooms about topics that you like to talk about with
-        others in which interested people can hop in and share thier interest.
-      </p>
-      <p>
-        You can also personally message if you don't want it to share in the
-        room.
-      </p>
-      <h3>Features</h3>
-      <ul>
-        <li>Create rooms</li>
-        <li>Direct Messages</li>
-        <li>Show all the users in the current room</li>
-        <li>Right side upper section can select any user to DM</li>
-      </ul>
-      <Link className="home-links" to="/signin">
-        Sign In
-      </Link>
-      <Link className="home-links" to="/protected">
-        Chats
-      </Link>
-      <h3>Responsive UI</h3>
-      <h3>Responsive UI is not completed.</h3>
-      <p>
-        So If you will open it on a phone it will not look or work as expected
-      </p>
+      <div className="details-cont">
+        <h1>Welcome to Let's Chat app.</h1>
+        <p>
+          Here you can create rooms about topics that you like to talk about
+          with others in which interested people can hop in and share thier
+          interest.
+        </p>
+        <p>
+          You can also personally message if you don't want it to share in the
+          room.
+        </p>
+        <h3>Features</h3>
+        <ul>
+          <li>Create rooms</li>
+          <li>Direct Messages</li>
+          <li>Show all the users in the current room</li>
+          <li>Right side upper section can select any user to DM</li>
+        </ul>
+        <Link className="home-links" to="/signin">
+          Sign In
+        </Link>
+        <Link className="home-links" to="/protected">
+          Chats
+        </Link>
+      </div>
+      <div className="illust-cont">
+        <img src="./undraw_online_chat_d7ek.svg" alt="chat-illustartion" />
+      </div>
     </div>
   );
 }
